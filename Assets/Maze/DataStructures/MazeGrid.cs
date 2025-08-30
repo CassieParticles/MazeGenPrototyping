ee4c1fd7f6777d458b5345b943a5f72ef62faa8f
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-08-30 01:31:50 +0100

    Added basic graph functionality

--- Assets/Maze/DataStructures/MazeGrid.cs
+++ Assets/Maze/DataStructures/MazeGrid.cs
@@ -1,68 +1,50 @@
+using System;
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
 
 namespace Maze
 {
-    public struct Node
-    {
-        public Node(Vector2Int position, byte connectionFlags, ref MazeGrid mazeGrid)
-        {
-            connections = new int[4];
-            this.connectionFlags = connectionFlags;
-        }
-
-        private byte connectionFlags;
-        public bool RightConnection { get => (connectionFlags & 0b10000000) == 0b10000000;}
-        public bool UpConnection { get => (connectionFlags & 0b01000000) == 0b01000000;}
-        public bool LeftConnection { get => (connectionFlags & 0b00100000) == 0b00100000;}
-        public bool DownConnection { get => (connectionFlags & 0b00010000) == 0b00010000;}
-
-        private int[] connections;  //Right, up, left, down
-        public int RightNode { get => connections[0]; set { connections[0] = value; } }
-        public int UpNode { get => connections[1]; set { connections[1] = value; } }
-        public int LeftNode { get => connections[2]; set { connections[2] = value; } }
-        public int DownNode { get => connections[3]; set { connections[3] = value; } }
-
-    }
-
     public class MazeGrid
     {
-        private Node[] nodeGrid;
+
+        private MazeNode[] nodeArray;
 
         public int width { get; private set; }
-        public int height { get; private set; }
+        public int height{ get; private set; }
 
         public MazeGrid(int width, int height)
         {
-            nodeGrid = new Node[width * height];
+            nodeArray = new MazeNode[width * height];
+            this.width = width;
+            this.height = height;
         }
 
-        public ref Node GetNode(Vector2Int position)
+        public MazeNode AddNode(MazeNode node, Vector2Int position)
         {
-            return ref GetNode(GetIndexFromPosition(position));
+            return AddNode(node, IndexFromPosition(position));
         }
 
-        public ref Node GetNode(int index)
+        public MazeNode AddNode(MazeNode node, int index)
         {
-            return ref nodeGrid[index];
+            return nodeArray[index] = node;
         }
 
-        public void SetNode(Node node, Vector2Int position)
+        public MazeNode GetNode(Vector2Int position)
         {
-            SetNode(node, GetIndexFromPosition(position));
+            return GetNode(IndexFromPosition(position));
         }
 
-        public void SetNode(Node node, int index)
+        public MazeNode GetNode(int index)
         {
-            nodeGrid[index] = node;
+            return nodeArray[index];
         }
 
-        public int GetIndexFromPosition(Vector2Int position)
+        public int IndexFromPosition(Vector2Int position)
         {
             return position.y * width + position.x;
-            
         }
 
-        public Vector2Int GetPositionFromIndex(int index)
+        public Vector2Int PositionFromIndex(int index)
         {
             return new Vector2Int(index % width, index / width);
         }
--- Assets/Maze/MazeGenerator.cs
+++ Assets/Maze/MazeGenerator.cs
@@ -1,14 +1,22 @@
+using Maze;
 using UnityEngine;
 
 public class MazeGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject[] RoomTemplates;
 
-    private Maze.Graph graph;
+    private Graph mazeGraph;
 
     private void Awake()
     {
-        graph = new Maze.Graph(10, 10, Vector2Int.zero);
+        mazeGraph = new Graph(10, 10);
+
+        mazeGraph.AddNode(new MazeNode(), new Vector2Int(3, 4));
+        mazeGraph.AddNode(new MazeNode(), new Vector2Int(3, 5));
+        mazeGraph.AddNode(new MazeNode(), new Vector2Int(3, 6));
+        mazeGraph.AddNode(new MazeNode(), new Vector2Int(4, 5));
+        mazeGraph.AddNode(new MazeNode(), new Vector2Int(5, 5));
+        mazeGraph.AddNode(new MazeNode(), new Vector2Int(3, 1));
     }
     void Start()
     {
