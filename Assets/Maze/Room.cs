7da119f7cb38b75406b7328d334e787b30634f70
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-09-05 01:52:14 +0100

    Removed redundant Room script

--- Assets/Maze/Room.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Room : MonoBehaviour
-{
-    public bool RightAccessible = true;
-    public bool UpAccessible = true;
-    public bool LeftAccessible = true;
-    public bool DownAccessible = true;
-}
